54c97755dec2ee1af8eab216df080b1e416fe4d4
aditiajanuar <aditia.januar@mhs.itenas.ac.id>
2026-01-08 14:46:53 +0700

    Message

---
+++ AritmatikaApp/src/aritmatikaapp/Operation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aritmatikaapp;
+
+/**
+ *
+ * @author Hype AMD
+ */
+public class Operation {
+    public double add(double a, double b){
+    return
+            a * b;
+    }
+}
